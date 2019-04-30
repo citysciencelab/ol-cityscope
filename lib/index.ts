d4abe0b6b8f5dbf9164c23c8bfc3fabd01fb60da
Jonas <jonas.luft@hcu-hamburg.de>
2019-04-30 17:31:49 +0200

    cleanup

--- lib/index.ts
+++ lib/index.ts
@@ -613,7 +613,7 @@ export function styleConfigToStyleFunction(style: LayerStyle, scale: { [key: str
     stroke: style.stroke ? getStroke(feature) : undefined,
     image: style.circle ? new CircleStyle({
       radius: style.circle.radius,
-      fill: style.fill ? getFill(feature) : new FillStyle(style.circle.fill),//new FillStyle(style.circle.fill),
+      fill: style.fill ? getFill(feature) : new FillStyle(style.circle.fill),
       stroke: style.stroke ? getStroke(feature) : new StrokeStyle(style.circle.stroke)
     }) : style.icon ? new IconStyle({
       src: style.icon.src,
